b74c33798315c4c77761fadebff99e865a8bb526
Jaydee Ballaho <jaydee.ballaho@wmsu.edu.ph>
2022-06-02 19:44:20 +0800

    Update color

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<head>
 		<title>Digital Education | Login</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
-		<link rel="icon" type="image/png" href="ICS-min.png" />
+		<link rel="icon" type="image/png" href="WMSU-LOGO-min.ico" />
 		<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
 	</head>
 	<body>
@@ -18,7 +18,7 @@
 			<div class="center-table">
 				<div class="common-box">
 					<div class="box-image">
-						<img class="wmsu-image" src="ICS-min.png">
+						<img class="wmsu-image" src="wmsu-logo.png">
 						<div class="title">DIGITAL EDUCATION</div>
 					</div>
 					<?php if($_GET["errorcode"] == 3) { ?>
@@ -26,7 +26,7 @@
 							<span class="text-error">Invalid username and password. Try again.</span>
 						</div>
 					<?php } ?>
-					<form action="https://lms.wmsuics.tech/login/index.php" method="post">
+					<form action="https://lms.wmsuccs.com/login/index.php" method="post">
 						<div class="group">
 							<label>Email Address</label>
 							<input type="text" name="username" class="textinput" required placeholder="">
@@ -40,7 +40,7 @@
 						</div>
 					</form>
 					<div class="group forgot">
-						<a href="https://lms.wmsuics.tech/login/forgot_password.php/" id="forgot-password">Forgot Password?</a>
+						<a href="https://lms.wmsuccs.com/login/forgot_password.php/" id="forgot-password">Forgot Password?</a>
 					</div>
 				</div>
 			</div>
